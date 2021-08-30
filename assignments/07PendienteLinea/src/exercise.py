7889cc7fc3dd2ea04cb5da14dde52883a3d4ab80
Pablu25 <89756803+Pablu25@users.noreply.github.com>
2021-08-29 21:55:01 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,15 +1,6 @@
 def main():
     # escribe tu código abajo de esta línea
      
-X1 = 1.5
-Y1 = 2.2
-X2 = 2.3
-Y2 = 5.3
-
-Total = (Y2 - Y1) / (X2 - X1)
-
-print(Total) 
-   
 
 
 if __name__ == '__main__':
