9cb1c7a1ecb0c045df35409e5769b968fdf0efeb
Pablu25 <89756803+Pablu25@users.noreply.github.com>
2021-08-29 22:44:31 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,6 +1,6 @@
 def main():
     # escribe tu código abajo de esta línea
-       x1= float(input("Dame x1:"))
+     x1= float(input("Dame x1:"))
      y1= float(input("Dame y1:"))
      x2= float(input("Dame x2:"))
      y2= float(input("Dame y2:"))
